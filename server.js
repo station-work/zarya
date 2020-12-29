744b06b6addf5008fa5a75467fa28399cf19d2b9
Enieber Cunha <7907068+enieber@users.noreply.github.com>
2020-12-29 02:43:40 -0300

    fix erro runtime

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
 
 const HASURA_OPERATION = `
 mutation ($name: String!, $email: String!, $password: String!, $username: String!) {
@@ -20,56 +19,60 @@ mutation ($name: String!, $email: String!, $password: String!, $username: String
 
 // execute the parent operation in Hasura
 const execute = async (variables) => {
-  const hasuraSecret = process.env.HASURA_SECRET
+  const hasuraSecret = process.env.HASURA_SECRET;
   const meta = {
-    'Content-Type': 'application/json',
-    'x-hasura-admin-secret': hasuraSecret
+    "Content-Type": "application/json",
+    "x-hasura-admin-secret": hasuraSecret,
   };
-  const headers = new Headers(meta)
+  const headers = new Headers(meta);
   const fetchResponse = await fetch(
     "https://station-work.herokuapp.com/v1/graphql",
     {
-      method: 'POST',
-      headers: headers
+      method: "POST",
+      headers: headers,
       body: JSON.stringify({
         query: HASURA_OPERATION,
-        variables
-      })
+        variables,
+      }),
     }
   );
   const data = await fetchResponse.json();
-  console.log('DEBUG: ', data);
+  console.log("DEBUG: ", data);
   return data;
 };
 
 // generate password in hash
 const generatePassword = async (password) => {
-  const salt = await bcrypt.genSalt()
-  const hash = await bcrypt.hash(password, salt)
-  
-  return hash
-}
+  const salt = await bcrypt.genSalt();
+  const hash = await bcrypt.hash(password, salt);
 
-app.post('/signup', async (req, res) => {
+  return hash;
+};
+
+app.post("/signup", async (req, res) => {
   // get request input
   const { name, email, password, username } = req.body.input;
 
   // run some business logic
-  const hash = await generatePassword(password)
- 
- 
+  const hash = await generatePassword(password);
+
   // execute the Hasura operation
-  const { data, errors } = await execute({ name, email, password: hash, username });
+  const { data, errors } = await execute({
+    name,
+    email,
+    password: hash,
+    username,
+  });
 
   // if Hasura operation errors, then throw error
   if (errors) {
-    return res.status(400).json(errors[0])
+    return res.status(400).json(errors[0]);
   }
 
   // success
   return res.json({
-    ...data.insert_user_one
-  })
+    ...data.insert_user_one,
+  });
 });
 
 app.listen(PORT);
