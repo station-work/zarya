cc2e7bf647361b165a6e1af1cf2232a4d8168c85
Enieber Cunha <eniebercunha@gmail.com>
2020-12-30 23:18:44 -0300

    feat: implement jwt in login and signup

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+require('dotenv').config()
 
 const app = express();
 
@@ -9,16 +12,27 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-const HASURA_OPERATION = `
-mutation ($name: String!, $email: String!, $password: String!, $username: String!) {
-  insert_user_one(object: {name: $name, email: $email, password: $password, username: $username}) {
-    id
+const HASURA_SIGNUP = `
+  mutation ($name: String!, $email: String!, $password: String!, $username: String!) {
+    insert_users_one(object: {name: $name, email: $email, password: $password, username: $username}) {
+      id
+    }
   }
-}
 `;
 
+const HASURA_LOGIN = `
+  query login($email: String!) {
+    users(where:{ email: {_eq: $email}}) {
+      email,
+      password,
+      id,
+      name
+    }
+  }
+`
+
 // execute the parent operation in Hasura
-const execute = async (variables) => {
+const execute = async (variables, query) => {
   const hasuraSecret = process.env.HASURA_SECRET;
   const meta = {
     "Content-Type": "application/json",
@@ -31,13 +45,12 @@ const execute = async (variables) => {
       method: "POST",
       headers: headers,
       body: JSON.stringify({
-        query: HASURA_OPERATION,
+        query,
         variables,
       }),
     }
   );
   const data = await fetchResponse.json();
-  console.log("DEBUG: ", data);
   return data;
 };
 
@@ -48,6 +61,11 @@ const generatePassword = async (password) => {
 
   return hash;
 };
+
+const comparaPassword = async (password, hashPassword) => {
+  const isMatch = await bcrypt.compare(password, hashPassword)
+  return isMatch
+}
 
 app.post("/signup", async (req, res) => {
   // get request input
@@ -62,17 +80,76 @@ app.post("/signup", async (req, res) => {
     email,
     password: hash,
     username,
-  });
+  }, HASURA_SIGNUP);
 
   // if Hasura operation errors, then throw error
   if (errors) {
     return res.status(400).json(errors[0]);
   }
 
+  const tokenContents = {
+    sub: data.insert_users_one.id.toString(),
+    name: name,
+    iat: Date.now() / 1000,
+    iss: 'https://myapp.com/',
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-user-id": data.insert_users_one.id.toString(),
+      "x-hasura-default-role": "user",
+      "x-hasura-role": "user"
+    },
+    exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60)
+  }
+
+  const token = jwt.sign(tokenContents, process.env.ENCRYPTION_KEY);
+
   // success
   return res.json({
-    ...data.insert_user_one,
+    ...data.insert_users_one,
+    token: token
   });
 });
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body.input;
+  // execute the Hasura operation
+  const { data, errors } = await execute({    
+    email,
+  }, HASURA_LOGIN);
+
+  // if Hasura operation errors, then throw error
+  if (errors) {
+    return res.status(400).json(errors[0]);
+  }
+
+  const isMatch = await comparaPassword(password, data.users[0].password)
+  if (!isMatch) {
+    return res.status(401).json({
+      message: `email or password is incorrect`
+    });
+  }
+
+
+  const tokenContents = {
+    sub: data.users[0].id.toString(),
+    name: data.users[0].name,
+    iat: Date.now() / 1000,
+    iss: 'https://myapp.com/',
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-user-id": data.users[0].id.toString(),
+      "x-hasura-default-role": "user",
+      "x-hasura-role": "user"
+    },
+    exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60)
+  }
+
+  const token = jwt.sign(tokenContents, process.env.ENCRYPTION_KEY);
+
+  return res.status(200).json({
+    id: data.users[0].id,
+    token: token
+  });
+})
 
 app.listen(PORT);
