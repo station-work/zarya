7c0514ea15b3a685fee97d9f937b74d56e83f5c8
Enieber Cunha <eniebercunha@gmail.com>
2021-04-08 18:30:52 -0300

    feat: create config to use in docker

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const ZARYA_PORT = process.env.ZARYA_PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -33,14 +33,15 @@ const HASURA_LOGIN = `
 
 // execute the parent operation in Hasura
 const execute = async (variables, query) => {
-  const hasuraSecret = process.env.HASURA_SECRET;
+  const hasuraSecret = process.env.HASURA_GRAPHQL_ADMIN_SECRET;
   const meta = {
     "Content-Type": "application/json",
     "x-hasura-admin-secret": hasuraSecret,
   };
   const headers = meta
+  const baseUrl = process.env.HASURA_APP_URL
   const fetchResponse = await fetch(
-    "https://station-work.herokuapp.com/v1/graphql",
+    `${baseUrl}/v1/graphql`,
     {
       method: "POST",
       headers: headers,
@@ -68,88 +69,105 @@ const comparaPassword = async (password, hashPassword) => {
 }
 
 app.post("/signup", async (req, res) => {
-  // get request input
-  const { name, email, password, username } = req.body.input;
+  try {
+    if (!req.body.input) {
+      return res.status(400).json({
+        message: "is required input object"
+      });
+    }
+    // get request input
+    const { name, email, password, username } = req.body.input;
+    // run some business logic
+    const hash = await generatePassword(password);
 
-  // run some business logic
-  const hash = await generatePassword(password);
+    // execute the Hasura operation
+    const { data, errors } = await execute({
+      name,
+      email,
+      password: hash,
+      username,
+    }, HASURA_SIGNUP);
+    // if Hasura operation errors, then throw error
+    if (errors) {
+      return res.status(400).json(errors[0]);
+    }
 
-  // execute the Hasura operation
-  const { data, errors } = await execute({
-    name,
-    email,
-    password: hash,
-    username,
-  }, HASURA_SIGNUP);
+    const tokenContents = {
+      sub: data.insert_users_one.id.toString(),
+      name: name,
+      iat: Date.now() / 1000,
+      iss: 'https://myapp.com/',
+      "https://hasura.io/jwt/claims": {
+        "x-hasura-allowed-roles": ["user"],
+        "x-hasura-user-id": data.insert_users_one.id.toString(),
+        "x-hasura-default-role": "user",
+        "x-hasura-role": "user"
+      },
+      exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60)
+    }
 
-  // if Hasura operation errors, then throw error
-  if (errors) {
-    return res.status(400).json(errors[0]);
+    const token = jwt.sign(tokenContents, process.env.ENCRYPTION_KEY);
+
+    // success
+    return res.json({
+      ...data.insert_users_one,
+      token: token
+    });
+  } catch (err) {
+    return res.status(500).json(err);
   }
-
-  const tokenContents = {
-    sub: data.insert_users_one.id.toString(),
-    name: name,
-    iat: Date.now() / 1000,
-    iss: 'https://myapp.com/',
-    "https://hasura.io/jwt/claims": {
-      "x-hasura-allowed-roles": ["user"],
-      "x-hasura-user-id": data.insert_users_one.id.toString(),
-      "x-hasura-default-role": "user",
-      "x-hasura-role": "user"
-    },
-    exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60)
-  }
-
-  const token = jwt.sign(tokenContents, process.env.ENCRYPTION_KEY);
-
-  // success
-  return res.json({
-    ...data.insert_users_one,
-    token: token
-  });
 });
 
 app.post("/login", async (req, res) => {
-  const { email, password } = req.body.input;
-  // execute the Hasura operation
-  const { data, errors } = await execute({    
-    email,
-  }, HASURA_LOGIN);
+  try {
+    const { email, password } = req.body.input;
+    // execute the Hasura operation
+    const { data, errors } = await execute({
+      email,
+    }, HASURA_LOGIN);
 
-  // if Hasura operation errors, then throw error
-  if (errors) {
-    return res.status(400).json(errors[0]);
-  }
+    // if Hasura operation errors, then throw error
+    if (errors) {
+      return res.status(400).json(errors[0]);
+    }
 
-  const isMatch = await comparaPassword(password, data.users[0].password)
-  if (!isMatch) {
-    return res.status(401).json({
-      message: `email or password is incorrect`
+    const isMatch = await comparaPassword(password, data.users[0].password)
+    if (!isMatch) {
+      return res.status(401).json({
+        message: `email or password is incorrect`
+      });
+    }
+
+
+    const tokenContents = {
+      sub: data.users[0].id.toString(),
+      name: data.users[0].name,
+      iat: Date.now() / 1000,
+      iss: 'https://myapp.com/',
+      "https://hasura.io/jwt/claims": {
+        "x-hasura-allowed-roles": ["user"],
+        "x-hasura-user-id": data.users[0].id.toString(),
+        "x-hasura-default-role": "user",
+        "x-hasura-role": "user"
+      },
+      exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60)
+    }
+
+    const token = jwt.sign(tokenContents, process.env.ENCRYPTION_KEY);
+
+    return res.status(200).json({
+      id: data.users[0].id,
+      token: token
     });
+  } catch (err) {
+    if (err instanceof TypeError) {
+      return res.status(400).json(err.message);
+    }
+    if (err.message) {
+      return res.status(500).json(err.message);
+    }
+    return res.status(500).json("Internal error");
   }
-
-
-  const tokenContents = {
-    sub: data.users[0].id.toString(),
-    name: data.users[0].name,
-    iat: Date.now() / 1000,
-    iss: 'https://myapp.com/',
-    "https://hasura.io/jwt/claims": {
-      "x-hasura-allowed-roles": ["user"],
-      "x-hasura-user-id": data.users[0].id.toString(),
-      "x-hasura-default-role": "user",
-      "x-hasura-role": "user"
-    },
-    exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60)
-  }
-
-  const token = jwt.sign(tokenContents, process.env.ENCRYPTION_KEY);
-
-  return res.status(200).json({
-    id: data.users[0].id,
-    token: token
-  });
 })
 
-app.listen(PORT);
+app.listen(ZARYA_PORT);
